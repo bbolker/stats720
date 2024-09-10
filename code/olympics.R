b0ab94e055e30622d5b31d5c6c2d6387dd7a5369
Ben Bolker <bbolker@gmail.com>
2024-09-10 19:52:55 -0400

    hw1

--- code/olympics.R
+++ code/olympics.R
@@ -9,44 +9,47 @@ medals <- athletes |>
     mutate(across(Team, ~ stringr::str_remove(., "-[0-9]+$"))) |>
     count(Team, Year, Medal)|>
     drop_na(Medal) |>
-    complete(Team, Year, Medal, fill = list(n=0))
-
+    complete(Team, Year, Medal, fill = list(n=0)) |>
+    rename_with(tolower)
+    
 wbd <- wb_data(c("NY.GDP.MKTP.KD", "SP.POP.TOTL"),
                start_date = 2000, end_date = 2024) |>
-    filter(date %in% unique(medals$Year))
+    filter(date %in% unique(medals$year))
 
-wbd2 <- wbd |> select(Team = country, Year = date, GDP = NY.GDP.MKTP.KD,
+wbd2 <- wbd |> select(team = country, year = date, gdp = NY.GDP.MKTP.KD,
                       pop = SP.POP.TOTL)
 
-match_vals <- read_delim("team_match.csv", delim = ";", trim_ws=TRUE)
-
 ## ugh, matching team names against country names ...
-m <- match(wbd2$Team, match_vals$Country)
-wbd3 <- wbd2 |>
-    mutate(across(Team,
-                  ~ ifelse(is.na(m), ., match_vals$Team[m])))
+## 'country' matches WB data; 'team' matches Olympic data
+match_vals <- read_csv("team_match.csv")
 
-olymp1 <- full_join(medals, wbd3, by = c("Team", "Year")) |>
+
+m <- match(wbd2$team, match_vals$country)
+wbd3 <- wbd2 |>
+    mutate(across(team,
+                  ~ ifelse(is.na(m), ., match_vals$team[m])))
+
+olymp1 <- full_join(medals, wbd3, by = c("team", "year")) |>
     mutate(pop = pop/1e6,
-           GDP = GDP/1e9)
+           gdp = gdp/1e9)
 
 ## diagnosing match problems
 ## find top populations with no medals
-olymp1 |> filter(is.na(n)) |> 
-    select(Team, pop) |>
-    group_by(Team) |>
+olymp1 |>
+    filter(is.na(n)) |> 
+    select(team, pop) |>
+    group_by(team) |>
     summarise(across(pop, mean)) |>
     arrange(desc(pop)) |>
     print(n = 20)
 
-## find top medal teams with no pop
+## find top gold medal teams with no pop
 olymp1 |>
-    filter(is.na(pop), Medal == "Gold") |>
-    select(Team, n) |>
-    group_by(Team)|>
+    filter(is.na(pop), medal == "Gold") |>
+    select(team, n) |>
+    group_by(team)|>
     summarise(across(n, mean)) |>
     arrange(desc(n)) |>
     print(n=20)
-
 
 write.csv(olymp1, "olymp1.csv", row.names=FALSE)
